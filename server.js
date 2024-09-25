227238803d8e0cea87edd7b523e3e6b4ccb6e945
Ruthrapathi <ruthrapathimurugan@outlook.com>
2024-09-25 23:31:37 +0530

    dada

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 const authRoutes = require('./routes/auth');
 const employeeRoutes = require('./routes/employee');
-const helmet = require('helmet'); // For security
+const path = require('path');
+
 
 const app = express();
 require('dotenv').config();
-
-// Apply security headers
-app.use(helmet());
-
-// CORS configuration
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://papaya-praline-c53106.netlify.app'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type, Authorization',
-}));
-
+app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve uploaded files
+app.use('/uploads', express.static('uploads')); // Serve uploaded files
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
+  
+  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeeRoutes);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
