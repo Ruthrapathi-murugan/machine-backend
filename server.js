5cfdcce566fcd54ce65164d3d1273d5d453cfdf2
Ruthrapathi <ruthrapathimurugan@outlook.com>
2024-09-25 23:32:45 +0530

    zz

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/uploads', express.static('uploads')); // Serve uploaded files
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-  
+
   app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
@@ -24,4 +24,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeeRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running  on port ${PORT}`));
