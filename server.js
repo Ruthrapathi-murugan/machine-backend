edb25c7522b6c03e6c099c1500d87a55753df977
Ruthrapathi <ruthrapathimurugan@outlook.com>
2024-09-25 11:14:27 +0530

    ss

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/uploads', express.static('uploads')); // Serve uploaded files
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB  connected'))
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Routes
